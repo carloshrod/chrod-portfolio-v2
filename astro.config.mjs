d28c3c2def9a3947fb2686169f43cdfa91a3683c
chrod <chrod.dev@gmail.com>
2026-03-16 19:11:29 -0500

    chore: add @astrojs/netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,15 @@ import { defineConfig } from "astro/config";
 import { loadEnv } from "vite";
 
 import tailwindcss from "@tailwindcss/vite";
-
 import react from "@astrojs/react";
-
 import sanity from "@sanity/astro";
+import netlify from "@astrojs/netlify";
 
 const env = loadEnv(process.env.NODE_ENV ?? "development", process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: netlify(),
   vite: {
     plugins: [tailwindcss()],
   },
